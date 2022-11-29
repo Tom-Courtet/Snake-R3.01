ff43c59d38fa24acdbe74d34d91189796dcb6852
Tom Courtet <tom.courtet@etu.univ-lyon1.fr>
2022-11-29 18:25:39 +0100

    Réparation erreur darkmode

--- darkmode.js
+++ darkmode.js
@@ -1,11 +1,11 @@
 const switchBox = document.getElementById("switch");
 const icone = document.querySelector("i");
 const link = document.getElementById("css-link");
-
 switchBox.addEventListener("click", change);
 
 function change() {
-    if (link.href === "http://127.0.0.1:5500/snake-js-s3/CSS/light-mode.css") {
+    console.log("salutt");
+    if (link.href.includes("CSS/light-mode.css")) {
         link.href = "CSS/dark-mode.css";
         icone.classList = "fas fa-sun";
         eColor = "#747d8c";
