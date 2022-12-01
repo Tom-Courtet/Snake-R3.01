013daffc461b1d893e921ccc3f49e2b7f6f93214
Tom Courtet <tom.courtet@etu.univ-lyon1.fr>
2022-12-01 15:45:31 +0100

    Stockage du mode de couleur

--- darkmode.js
+++ darkmode.js
@@ -2,19 +2,31 @@ const switchBox = document.getElementById("switch");
 const icone = document.querySelector("i");
 const link = document.getElementById("css-link");
 switchBox.addEventListener("click", change);
+console.log(localStorage.getItem("mode"));
+if (localStorage.getItem("mode") === null) {
+    localStorage.setItem("mode", "light-mode");
+} else if (localStorage.getItem("mode") === "light-mode") {
+    link.href = "CSS/light-mode.css";
+} else if (localStorage.getItem("mode") === "dark-mode") {
+    link.href = "CSS/dark-mode.css";
+}
 
 function change() {
-    if (link.href.includes("CSS/light-mode.css")) {
+    console.log(localStorage.getItem("mode"));
+    if (localStorage.getItem("mode") === "light-mode") {
+        localStorage.setItem("mode", "dark-mode");
         link.href = "CSS/dark-mode.css";
         icone.classList = "fas fa-sun";
         eColor = "#747d8c";
         hColor = "#dfe4ea";
         fColor = "#ff4757";
     } else {
+        localStorage.setItem("mode", "light-mode");
         link.href = "CSS/light-mode.css";
         icone.classList = "fas fa-moon";
         eColor = "lightblue";
         hColor = "#004721";
         fColor = "red";
     }
+    console.log(localStorage.getItem("mode"));
 }
